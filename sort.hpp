76a34d30817e689c9d9e7933385bfb692616f2d3
AnkilP <a293pate@uwaterloo.ca>
2020-02-08 15:59:39 -0500

    generic sorter

--- sort.hpp
+++ sort.hpp
@@ -1,19 +1,16 @@
-template<typename T>
-class compareNeighbours{
-    public:
-        int operator() (const T& p1, const T& p2)
-        {
-            return p1.getCost() > p2.getCost();
-        }
-};
+#include <algorithm>
+#include <vector>
+#include <queue>
 
+namespace sorting{
 
+  template<typename T, typename P>
+  class sorts {
+    T internal_data_store;
+    P comparator;
+  public:
+    sorts(const P(*compar)(const T &, const T &));
+    void sort(T&)
+  };
 
-template<typename T, typename P>
-class sorts {
-  T internal_data_store;
-  P comparator;
-public:
-  sorts(const P(*compar)(const T &, const T &));
-  void sort(T&)
-};
+}
