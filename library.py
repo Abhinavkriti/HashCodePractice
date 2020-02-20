618785acd5eae0ae39e9959055c82ba93057e8bd
Amour <karir.amour@gmail.com>
2020-02-20 14:36:31 -0500

    Fixing parameters, adding repr, adding used_books

--- library.py
+++ library.py
@@ -1,8 +1,10 @@
 maxDays = 4
-class Library():
-    def __init__(self, libId, books, scanDays, maxPerDay, numBooks):
+used_books = []
+
+class Library:
+    def __init__(self, libId, books, scanDays, maxPerDay):
         self.libId = libId;
-        self.numBooks = numBooks;
+        self.numBooks = len(books);
         self.books = books;
         #List of tuples [id, score]
         self.books.sort(key=lambda x: x[1], reverse=True);
@@ -10,12 +12,17 @@ class Library():
         self.maxPerDay = maxPerDay;
         self.score = 0;
 
+    def __repr__(self):
+        return "id: {0}\nbookCount: {1}\nrate: {2}\nsignup_time: {3}".format(
+            self.libId, self.numBooks, self.scanDays, self.maxPerDay
+    )
     def scoreLib(self):
         curIndex = 0
         #Maxdays constant for our deadline day
         for i in range(maxDays - self.scanDays):
             maxIndex = min(curIndex+self.maxPerDay, self.numBooks-1)
-            self.score += sum(score for _, score in self.books[curIndex, maxIndex])
+            include_books = (score for book, score in self.books[curIndex: maxIndex] if book not in used_books)
+            self.score += sum(include_books)
             curIndex = maxIndex
             #End if we've used all our books early.
             if curIndex >= self.numBooks:
--- testLib.py
+++ testLib.py
@@ -3,8 +3,9 @@ from library import Library
 def rankLibs(libs):
     libs.sort(key=lambda x: x.scoreLib(), reverse=True)
 
-lib1 = Library(0, [[0,5],[1,2],[4,3]], 3, 1, 3)
-lib2 = Library(1, [[0,5],[1,2],[5,10]], 1, 2, 3)
+lib1 = Library(0, [[0,5],[1,2],[4,3]], 3, 1)
+lib2 = Library(1, [[0,5],[1,2],[5,10]], 1, 2)
 libs = [lib1, lib2]
 rankLibs(libs)
-print('Ranked libs: ', libs)
+print(lib1)
+print('Ranked libs: ', list(libs))
