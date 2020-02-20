d39773878d8e7aaf974447df8d1109aa1243955e
Amour <karir.amour@gmail.com>
2020-02-20 14:53:33 -0500

    Fixed initial scoring function

--- library.py
+++ library.py
@@ -10,22 +10,15 @@ class Library:
         self.books.sort(key=lambda x: x[1], reverse=True);
         self.scanDays = scanDays;
         self.maxPerDay = maxPerDay;
-        self.score = 0;
+        self.score = self.scoreLib(daysLeft=maxDays, usedBooks=[])
 
     def __repr__(self):
         return "id: {0}\nbookCount: {1}\nrate: {2}\nsignup_time: {3}".format(
             self.libId, self.numBooks, self.scanDays, self.maxPerDay
     )
-    def scoreLib(self):
-        curIndex = 0
+
+    def scoreLib(self, daysLeft, usedBooks):
         #Maxdays constant for our deadline day
-        for i in range(maxDays - self.scanDays):
-            maxIndex = min(curIndex+self.maxPerDay, self.numBooks-1)
-            include_books = (score for book, score in self.books[curIndex: maxIndex] if book not in used_books)
-            self.score += sum(include_books)
-            curIndex = maxIndex
-            #End if we've used all our books early.
-            if curIndex >= self.numBooks:
-                return self.score;
-        return self.score;
+        include_books = (score for (book, score) in self.books[maxPerDay * (daysLeft - self.scanDays)] if book not in used_books)
+        self.score = sum(include_books)
 
--- testLib.py
+++ testLib.py
@@ -1,11 +1,12 @@
 from library import Library
 
 def rankLibs(libs):
-    libs.sort(key=lambda x: x.scoreLib(), reverse=True)
+    libs.sort(key=lambda x: x.score, reverse=True)
+
 
 lib1 = Library(0, [[0,5],[1,2],[4,3]], 3, 1)
 lib2 = Library(1, [[0,5],[1,2],[5,10]], 1, 2)
 libs = [lib1, lib2]
 rankLibs(libs)
-print(lib1)
+print(lib1.score)
 print('Ranked libs: ', list(libs))
